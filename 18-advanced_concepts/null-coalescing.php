55d0a17966c332f261b1bdaf79751c5db9be953e
X-DX <aruproy5637@gmail.com>
2025-01-21 13:50:03 +0530

    null-coalescing

--- 18-advanced_concepts/null-coalescing.php
+++ 18-advanced_concepts/null-coalescing.php
@@ -6,12 +6,16 @@
     <link rel="stylesheet" type="text/css" href="./simple.css" />
     <title>Document</title>
 </head>
-<body><pre><?php
+<body>
+    <pre>
+        <?php
+            // @: This is an error control operator, It suppresses any errors that might occur during the type casting or the null coalescing operation.
 
-$name = @(string) ($_GET['name'] ?? '');
-var_dump($name);
+            $name = @(string) ($_GET['name'] ?? '');
+            var_dump($name);
     
-?></pre>
-<a href="null-coalescing.php?<?php echo http_build_query(['name' => ['Jannis', 'Olivia']]); ?>">Link</a>
+        ?>
+    </pre>
+    <a href="null-coalescing.php?<?php echo http_build_query(['name' => ['Jannis', 'Olivia']]); ?>">Link</a>
 </body>
 </html>
