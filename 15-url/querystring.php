afa92faf985e1cce6ca2eca2c4b9e3077ec84699
X-DX <aruproy5637@gmail.com>
2025-01-21 12:56:56 +0530

    http_build_query

--- 15-url/querystring.php
+++ 15-url/querystring.php
@@ -9,6 +9,8 @@
     <pre>
         <!-- <a href="querystring.php?book=Harry Potter">Harry Potter</a>
         <a href="querystring.php?book=Beauty & the Beast">Beauty & the Beast</a> -->
+
+        <!-- http_build_query(): used to generate URL-encoded query string from the associative (or indexed) array. -->
         
         <a href="querystring.php ? <?php echo http_build_query(['book' => 'Harry Potter']); ?>">Harry Potter</a>
         <a href="querystring.php ? <?php echo http_build_query(['book' => 'Beauty & the Beast', 'firstname' => 'Arup']); ?>">Beauty & the Beast</a>
